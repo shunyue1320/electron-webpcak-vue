7c4f7ca2c582c6f127c1b60e146d11f8e8f1d5c6
shunyue1320 <1277952981@qq.com>
2020-01-10 14:10:54 +0800

    配置 process.env

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  mode: process.env.NODE_ENV == 'development' ? 'development' : 'production',
   target: 'electron-main',
   entry: { main: './src/main/index.js' },
   output: {
